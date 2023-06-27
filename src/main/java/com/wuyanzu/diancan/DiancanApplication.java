29cfc0150a6f19055760092d441d12677b73529a
二狗 <sinclair3546@gmail.com>
2023-06-27 11:22:22 +0800

    testing

---
+++ src/main/java/com/wuyanzu/diancan/DiancanApplication.java
@@ -0,0 +1,13 @@
+package com.wuyanzu.diancan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiancanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiancanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wuyanzu/diancan/DiancanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuyanzu.diancan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiancanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
