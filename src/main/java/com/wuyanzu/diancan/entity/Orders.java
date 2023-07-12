2883b4ad19445354d2894858496b0930cc934e05
二狗 <sinclair3546@gmail.com>
2023-07-12 16:23:42 +0800

    时区设置

--- src/main/java/com/wuyanzu/diancan/entity/Orders.java
+++ src/main/java/com/wuyanzu/diancan/entity/Orders.java
@@ -30,7 +30,7 @@ public class Orders {
 
     private String remark;              //订单备注
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
     @TableField(value = "createtime")
     private Timestamp createTime;           //下单时间
 }
